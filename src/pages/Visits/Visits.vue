774282285d0842ddfdb3f59492eee761f2e70bf0
golima <lima01@gmail.com>
2021-06-03 17:24:40 -0400

    Use date picker

--- src/pages/Visits/Visits.vue
+++ src/pages/Visits/Visits.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="visits-page">
-    <h1 class="page-title">Hate Crime Map</h1>
+    <b-row>
+      <b-col lg="6" xs="12">
+        <h1 class="page-title">Hate Crime Map</h1>
+      </b-col>
+      <b-col lg="6" xs="12">
+        <date-range-picker
+          ref="picker"
+          :locale-data="{ firstDay: 1, format: 'dd-mm-yyyy' }"
+          minDate="1/1/2010"
+          :singleDatePicker="false"
+          :showDropdowns="true"
+          :autoApply="true"
+          v-model="dateRange"
+          @update="updateValues"
+        >
+          <template v-slot:input="picker" style="min-width: 350px">
+            {{ formatDate(picker.startDate) }} -
+            {{ formatDate(picker.endDate) }}
+          </template>
+        </date-range-picker>
+      </b-col>
+    </b-row>
     <b-row>
       <b-col lg="12">
         <Widget
@@ -8,7 +29,7 @@
           customHeader
         >
           <div class="bg-transparent" style="align: center">
-            <Map style="width: 80%" :stats=mapStats />
+            <Map style="width: 80%" :stats="mapStats" />
           </div>
         </Widget>
       </b-col>
@@ -28,14 +49,14 @@
                 :series="incidentData.series"
               ></apexchart>
             </div>
-            <div id="chart-line">
+            <!-- <div id="chart-line">
               <apexchart
-                type="bar"
-                height="150"
+                type="area"
+                height="100"
                 :options="incidentData.chartOptionsLine"
                 :series="incidentData.seriesLine"
               ></apexchart>
-            </div>
+            </div> -->
           </div>
         </Widget>
       </b-col>
@@ -54,32 +75,13 @@
                     {{ row.incident_location }}
                   </td>
                   <td>
-                    <a :href=row.url target="_blank" :title=row.abstract >{{ row.title }}</a>
+                    <a :href="row.url" target="_blank" :title="row.abstract">{{
+                      row.title
+                    }}</a>
                   </td>
                 </tr>
               </tbody>
             </table>
-          </div>
-          <div class="clearfix">
-            <div class="float-right">
-              <b-button variant="default" class="mr-3" size="sm"
-                >Send to...</b-button
-              >
-              <b-dropdown
-                variant="inverse"
-                class="mr-xs"
-                size="sm"
-                text="Clear"
-                right
-              >
-                <b-dropdown-item>Clear</b-dropdown-item>
-                <b-dropdown-item>Move ...</b-dropdown-item>
-                <b-dropdown-item>Something else here</b-dropdown-item>
-                <b-dropdown-divider />
-                <b-dropdown-item>Separated link</b-dropdown-item>
-              </b-dropdown>
-            </div>
-            <p>Basic table with styled content</p>
           </div>
         </Widget>
       </b-col>
@@ -95,7 +97,9 @@ import AreaChart from "./components/AreaChart/AreaChart";
 import AnimatedNumber from "animated-number-vue";
 import config from "../../config";
 import axios from "axios";
-import moment from 'moment'
+import moment from "moment";
+import DateRangePicker from "vue2-daterange-picker";
+import "vue2-daterange-picker/dist/vue2-daterange-picker.css";
 const colors = config.colors;
 
 export default {
@@ -105,10 +109,14 @@ export default {
     Widget,
     AreaChart,
     AnimatedNumber,
+    DateRangePicker,
   },
   data() {
-    const data = [];
     return {
+      dateRange: {
+        startDate: moment().subtract(1, "year"),
+        endDate: moment(),
+      }, //range of data to show result
       animateNumberOptions: {
         duration: 2000,
         easing: "easeInQuad",
@@ -119,12 +127,8 @@ export default {
       checkedArr: [false, false, false],
       dataCollection: null,
       incidentData: {
-        series: [
-          {
-            data: data,
-          },
-        ],
-        mapStats:{},
+        series: [],
+        mapStats: {},
         chartOptions: {
           chart: {
             id: "chart2",
@@ -175,11 +179,7 @@ export default {
           },
         },
 
-        seriesLine: [
-          {
-            data: data,
-          },
-        ],
+        seriesLine: [],
         chartOptionsLine: {
           chart: {
             id: "chart1",
@@ -191,9 +191,15 @@ export default {
             },
             selection: {
               enabled: true,
+              fill: {
+                color: "#ffffff",
+                opacity: 0.5,
+              },
               xaxis: {
-                // min: new Date("19 Jun 2010").getTime(),
-                // max: new Date("14 Aug 2017").getTime(),
+                // min: new Date("1 Jan 2020").getTime(),
+                // max: new Date().getTime(),
+                // min: parseInt(moment().subtract(1, "year").format()),
+                // max: parseInt(moment().format()),
                 labels: {
                   style: {
                     colors: colors.chartTextColor,
@@ -226,11 +232,13 @@ export default {
           },
         },
       },
-      incidentList: [
-      ],
+      incidentList: [],
     };
   },
   methods: {
+    updateValues(dateRange) {
+      this.fillData(dateRange.startDate, dateRange.endDate);
+    },
     checkTable(id) {
       let arr = [];
       if (id === 0) {
@@ -261,63 +269,89 @@ export default {
         dateSet[2]
       }, ${dateSet[3]}`;
     },
-    formatDate(dateString){ //format to mm/dd/yyyy
-      return moment(String(dateString)).format('MM/DD/YYYY')
+    formatDate(dateString) {
+      //format to mm/dd/yyyy
+      if (!dateString) return "Date";
+      return moment(dateString).format("MM/DD/YYYY");
     },
-    fillData() {
-      axios.get(config.api_endpoint+"/incidents")
-          .then((response) => {
-            this.incidentList = response.data.incidents
-          })
-      axios.get(config.api_endpoint+"/stats")
-          .then((response) => {
-            const dailyCountSeries = []
-            const monthlyCountSeries = []
-            let currMonth=0
-            let monthTotal = 0
-            response.data.stats.forEach(dailyCount => {
-              dailyCountSeries.push([moment(String(dailyCount.key)).format(), dailyCount.value])
-              const month = moment(String(dailyCount.key)).startOf('month').format()
-              if (currMonth==0) {
-                currMonth=month
-              }
-              if (currMonth == month ) {
-                monthTotal += dailyCount.value
-              }
-              else {
-                monthlyCountSeries.push([currMonth, monthTotal])
-                currMonth = month
-                monthTotal = dailyCount.value
-              }
-            });
-            if ( 0!=currMonth){
-              monthlyCountSeries.push([currMonth, monthTotal])
-            }
-            this.incidentData.series =  [
-                  {
-                    data: dailyCountSeries,
-                  },
-            ]
-            this.incidentData.seriesLine = [
-                  {
-                    data: monthlyCountSeries,
-                  },
-            ]
-            //Data to populate map
-            /* 
+    fillData(startDate, endDate, state = null) {
+      const incidentsAPIUrl =
+        config.api_endpoint +
+        "/incidents?start=" +
+        moment(startDate).format("MM-DD-YYYY") +
+        "&end=" +
+        moment(endDate).format("MM-DD-YYYY") +
+        (state ? "&state=" + state : "");
+      axios.get(incidentsAPIUrl).then((response) => {
+        this.incidentList = response.data.incidents;
+      });
+      const statsAPIUrl =
+        config.api_endpoint +
+        "/stats?start=" +
+        moment(startDate).format("MM-DD-YYYY") +
+        "&end=" +
+        moment(endDate).format("MM-DD-YYYY") +
+        (state ? "&state=" + state : "");
+      axios.get(statsAPIUrl).then((response) => {
+        const dailyCountSeries = [];
+        const monthlyCountSeries = [];
+        let currMonth = 0;
+        let monthTotal = 0;
+        response.data.stats.forEach((dailyCount) => {
+          dailyCountSeries.push([
+            moment(String(dailyCount.key)).format(),
+            dailyCount.value,
+          ]);
+          const month = moment(String(dailyCount.key)).startOf("week").format();
+          if (currMonth == 0) {
+            currMonth = month;
+          }
+          if (currMonth == month) {
+            monthTotal += dailyCount.value;
+          } else {
+            monthlyCountSeries.push([currMonth, monthTotal]);
+            currMonth = month;
+            monthTotal = dailyCount.value;
+          }
+        });
+        if (0 != currMonth) {
+          monthlyCountSeries.push([currMonth, monthTotal]);
+        }
+        this.incidentData.series = [
+          {
+            data: dailyCountSeries,
+          },
+        ];
+        this.incidentData.seriesLine = [
+          {
+            data: monthlyCountSeries,
+          },
+        ];
+        this.incidentData.chartOptionsLine.chart.selection.xaxis.max =
+          monthlyCountSeries[monthlyCountSeries.length - 1][0];
+        this.incidentData.chartOptionsLine.chart.selection.xaxis.min = Math.max(
+          parseInt(
+            moment(this.incidentData.chartOptionsLine.chart.selection.xaxis.max)
+              .subtract(1, "year")
+              .format()
+          ), //max - 1 year
+          monthlyCountSeries[0][0]
+        );
+        //Data to populate map
+        /* 
             mapData: [
               { state: NY,
                 date_range: "Mar 2018 - Sep 2020",
                 total: 123
               }
             ]
-            */ 
-            this.mapStats = {"total":response.data.total}
-          })
-    } //fillData
+            */
+        this.mapStats = { total: response.data.total };
+      });
+    }, //fillData
   },
   mounted() {
-    this.fillData();
+    this.fillData(moment().subtract(1, "year"), moment());
   },
 };
 </script>
