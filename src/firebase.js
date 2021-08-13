87b1c9def2b571f0b39d44f954abca47c4dc5ccb
Li Ma <lima01@gmail.com>
2021-08-12 22:56:07 -0400

    Clean unused firebase import

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/firestore"
-import { functions } from "firebase"
 
 const firebaseConfig = {
     apiKey: "AIzaSyCdt-8N8Em3JHcMeEua-QMR3txkqdR01dg",
