560ed444fd4b4cfef6e9ec773d3df890f5ce975d
Li Ma <lima01@gmail.com>
2021-08-14 08:53:41 -0400

    Select state in map from dropdown and via map click.

--- src/views/IncidentMap.js
+++ src/views/IncidentMap.js
@@ -16,15 +16,12 @@ https://www.amcharts.com/docs/v4/getting-started/integrations/using-react/
 
 //mapData is result from api/stats.total
 const IncidentMap = (props) => {
-  console.log("init IncidentMap"  );
-  const [mapState, setMapState] = useState();
-  // const [mapPolygonSeries, setMapStatePolygonSeries] = useState();
-  // const [mapPolygonTemplate, setMapStatePolygonTemplate] = useState();
-  // const [currentActiveState, setCurrentActiveState] = useState();
-  // const [currentActiveStateZIndex, setCurrentActiveStateZIndex] = useState();
+  const [mapPolygonSeries, setMapPolygonSeries] = useState();
+  const [polygonTemplate, setPolygonTemplate] = useState();
+  const [currentActiveState, setCurrentActiveState] = useState();
 
   const updateMap = (mapStatistics) => {
-    if (!mapState || !mapState.mapPolygonSeries) return;
+    if (!mapPolygonSeries) return;
     let data = [];
     for (const state in mapStatistics) {
       data.push({
@@ -32,42 +29,37 @@ const IncidentMap = (props) => {
         value: mapStatistics[state]
       })
     }
-    mapState.mapPolygonSeries.data = data;
+    mapPolygonSeries.data = data;
   }
   useEffect(() => {
     updateMap(props.mapData);
   }, [props.mapData]);
 
   useEffect(() => {
-    if (!mapState || !mapState.mapPolygonSeries || !mapState.mapPolygonTemplate) return;
-    const polygon = mapState.mapPolygonSeries.getPolygonById("US-" + props.selectdState);
-    selectState(polygon);
+    if (!mapPolygonSeries || !polygonTemplate) return;
+    const polygon = mapPolygonSeries.getPolygonById("US-" + props.selectdState);
+    setCurrentActiveState(polygon);
   }, [props.selectdState]);
 
+  useEffect(() => {
+    console.log("currentActiveState changed");
+    selectState(currentActiveState);
+  }, [currentActiveState]);
+
   const selectState = (stateMapObject) => {
-    // if (currentActiveState == stateMapObject)
-    //   return;
-    // if (currentActiveState) { //clear the previousely selected state
-    //   currentActiveState.zIndex = currentActiveStateZIndex;
-    //   currentActiveState.strokeWidth = mapPolygonTemplate.strokeWidth;
-    //   currentActiveState.stroke = mapPolygonTemplate.stroke;
-    //   currentActiveState.filters.clear();
-    // }
-    if (mapState.mapPolygonSeries) {
-      for ( let i=0; i< mapState.mapPolygonSeries.mapPolygons.length; i++) {
-        const polygon = mapState.mapPolygonSeries.mapPolygons[i];
-        polygon.strokeWidth = mapState.mapPolygonTemplate.strokeWidth;
-        polygon.stroke = mapState.mapPolygonTemplate.stroke;
+    if (mapPolygonSeries && polygonTemplate) {
+      //clean selection effect on other states
+      for ( let i=0; i< mapPolygonSeries.mapPolygons.length; i++) {
+        const polygon = mapPolygonSeries.mapPolygons.values[i];
+        polygon.strokeWidth = polygonTemplate.strokeWidth;
+        polygon.stroke = polygonTemplate.stroke;
         polygon.filters.clear();
       }
     }
 
     //https://www.amcharts.com/docs/v4/tutorials/consistent-outlines-of-map-polygons-on-hover/
-
-    
     if (stateMapObject) {
-      //setCurrentActiveStateZIndex(stateMapObject.zIndex);
-      mapState.currentActiveStateZIndex = stateMapObject.zIndex
+      setCurrentActiveStateZIndex(stateMapObject.zIndex);
       stateMapObject.zIndex = Number.MAX_VALUE;
       stateMapObject.toFront();
       stateMapObject.strokeWidth = 3;
@@ -77,8 +69,6 @@ const IncidentMap = (props) => {
       activeShadow.dy = 6;
       activeShadow.opacity = 0.3;
     }
-    mapState.currentActiveState = stateMapObject;
-    setMapState(mapState);
   }
   //componentDidMount
   useLayoutEffect(() => {
@@ -117,21 +107,18 @@ const IncidentMap = (props) => {
     hs.properties.fillOpacity = 0.5;
 
     polygonTemplate.events.on("hit", function (ev) {
-      if (!mapState || mapState.currentActiveState == ev.target) return;
-      selectState(ev.target);
-      if (ev.target && ev.target.id) {
+      if (currentActiveState == ev.target) return;
+      //selectState(ev.target);
+      setCurrentActiveState(ev.target);
+      if ( ev.target && ev.target.id ) {
         props.onChange(ev.target.id.split("-")[1]);
       }
     });
 
     polygonTemplate.stroke = am4core.color("#6979C9");
     polygonTemplate.strokeOpacity = 1;
-
-    setMapState({
-      mapPolygonSeries: polygonSeries,
-      mapPolygonTemplate: polygonTemplate
-    });
-    // setMapPolygonTemplate(polygonTemplate);
+    setMapPolygonSeries(polygonSeries);
+    setPolygonTemplate(polygonTemplate);
     return () => {
       map.dispose();
     };
