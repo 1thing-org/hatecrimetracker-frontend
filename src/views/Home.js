4092fe10e89068c620b76f293ba06decea2df53c
httplife <me@httplife.com>
2021-08-24 00:21:44 -0700

    add useRouter, update uri, set default dateRange

--- src/views/Home.js
+++ src/views/Home.js
@@ -14,10 +14,18 @@ import IncidentMap from './IncidentMap'
 import StateSelection from './StateSelection'
 import UILoader from '@components/ui-loader'
 
+import { useRouter } from '@hooks/useRouter'
+
 const Home = () => {
+    const router = useRouter()
+    const defaultDateRange =
+        Object.entries(router.query).length === 0
+            ? [moment().subtract(1, 'years').toDate(), new Date()]
+            : [moment(router.query.from).toDate(), moment(router.query.to).toDate()]
+
     const [incidents, setIncidents] = useState([])
     const [selectedState, setSelectedState] = useState('')
-    const [dateRange, setDateRange] = useState([moment().subtract(1, 'years').toDate(), new Date()])
+    const [dateRange, setDateRange] = useState(defaultDateRange)
     const [incidentTimeSeries, setIncidentTimeSeries] = useState([])
     const [incidentAggregated, setIncidentAggregated] = useState([])
     const [loading, setLoading] = useState(false)
@@ -64,8 +72,13 @@ const Home = () => {
     }, [dateRange])
 
     const { colors } = useContext(ThemeColors)
+
+    // handle date change
     function handleDateRangeSelect(ranges) {
         setDateRange(ranges)
+        // console.log(ranges)
+
+        router.push(`/home?from=${moment(ranges[0]).format('YYYY-MM-DD')}&to=${moment(ranges[1]).format('YYYY-MM-DD')}`)
     }
 
     function onStateChange(state) {
@@ -80,10 +93,18 @@ const Home = () => {
                             <CardBody>
                                 <Container>
                                     <Row>
-                                        <Col xs="12" sm="auto"><FormGroup>
-                                            <Label>Location:</Label> <StateSelection value={selectedState} onChange={onStateChange} /> </FormGroup></Col>
-                                        <Col xs="12" sm="auto"><FormGroup>
-                                            <Label>Time Period:</Label> <DateRangeSelector onChange={handleDateRangeSelect} value={dateRange} /></FormGroup></Col>
+                                        <Col xs='12' sm='auto'>
+                                            <FormGroup>
+                                                <Label>Location:</Label>{' '}
+                                                <StateSelection value={selectedState} onChange={onStateChange} />{' '}
+                                            </FormGroup>
+                                        </Col>
+                                        <Col xs='12' sm='auto'>
+                                            <FormGroup>
+                                                <Label>Time Period:</Label>{' '}
+                                                <DateRangeSelector onChange={handleDateRangeSelect} value={dateRange} />
+                                            </FormGroup>
+                                        </Col>
                                     </Row>
                                 </Container>
                             </CardBody>
@@ -94,7 +115,11 @@ const Home = () => {
                     <Col xl='8' lg='8' md='6' xs='12'>
                         <Card>
                             <CardBody>
-                                <BarChart color={colors.primary.main} chart_data={incidentTimeSeries} state={selectedState} />
+                                <BarChart
+                                    color={colors.primary.main}
+                                    chart_data={incidentTimeSeries}
+                                    state={selectedState}
+                                />
                                 <IncidentMap
                                     mapData={incidentAggregated}
                                     selectdState={selectedState}
