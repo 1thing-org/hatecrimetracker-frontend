2e16d040e538b5bc5991a9b43ae0677d16144be5
Li <lma@cardwellbeach.com>
2021-10-27 09:15:49 -0400

    Add support of Myanmar

--- src/utility/Languages.js
+++ src/utility/Languages.js
@@ -9,17 +9,19 @@ export const BROWSER_LANGUAGES = {
     'ko': 'ko',
     'hi': 'hi',
     'si' :'si',
+    'my-MM': 'my-MM'
 };
 
 //Supported language id to native name
 export const SUPPORTED_LANGUAGES = {
     'en': 'English',
-    'zh-CN': '简体中文', 
-    'zh-TW': '繁體中文',
-    'ja': '日本語',
-    'ko': '한국어',
-    'hi': 'हिन्दी',
-    'si': 'සිංහල',
+    'zh-CN': '简体中文 (Chinese Simplified)', 
+    'zh-TW': '繁體中文 (Chinese Tranditional)',
+    'ja': '日本語 (Japanese)',
+    'ko': '한국어 (Korean)',
+    'hi': 'हिन्दी (Hindi)',
+    'si': 'සිංහල (Sinhalese)',
+    'my-MM': 'မြန်မာ (Myanmar)'
 };
 
 export const getBrowserLang = () => BROWSER_LANGUAGES[navigator.language] || 'en';
