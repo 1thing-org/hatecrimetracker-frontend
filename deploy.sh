bf73e07c455ee269ab71a092aa1ab641399509dc
Li Ma <lima01@gmail.com>
2021-10-27 22:35:52 -0400

    Fix Thai translation

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 cp src/configs/config_prod.js src/configs/appConfig.js
 yarn build
 gcloud config set project hate-crime-tracker
+gsutil rm -r build/* gs://hatecrimetracker/*
 gsutil cp -R build/* gs://hatecrimetracker
 cp src/configs/config_dev.js src/configs/appConfig.js
