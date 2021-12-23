ce83e368a12aa9b07869d7f71081ff03547d8d5b
Li Ma <lima01@gmail.com>
2021-12-23 11:20:43 -0500

    Refine and translate keyword search.

--- src/views/IncidentList.js
+++ src/views/IncidentList.js
@@ -3,78 +3,70 @@ import { useState, useEffect } from 'react'
 import { stateFullName } from '../utility/Utils.js'
 import { Button } from 'reactstrap'
 import { useTranslation } from 'react-i18next';
-import {Input} from 'rsuite';
+import { Input } from 'rsuite';
 
 const INCR_COUNT = 10;
 const IncidentList = (props) => {
     const { t } = useTranslation();
-    const [visibleCount, setVisibleCount] = useState(20);
+    const [visibleLimit, setVisibleLimit] = useState(20);
     useEffect(() => {
-        setVisibleCount(INCR_COUNT);
+        setVisibleLimit(INCR_COUNT);
     }, [props.data])
     const getTitle = (incident) => {
-        if ( incident?.title_translate ) {
-            for ( const [key, value] of Object.entries(incident.title_translate) ) {
+        if (incident?.title_translate) {
+            for (const [key, value] of Object.entries(incident.title_translate)) {
                 return value;
             }
         }
         return incident?.title;
     }
     const getAbstract = (incident) => {
-        if ( incident?.abstract_translate ) {
-            for ( const [key, value] of Object.entries(incident.abstract_translate) ) {
+        if (incident?.abstract_translate) {
+            for (const [key, value] of Object.entries(incident.abstract_translate)) {
                 return value;
             }
         }
         return incident?.abstract;
     }
-
+    let visibleCount = 0;
 
     const [searchTerm, setSearchTerm] = useState("")
 
     return (
         <div>
-            <Input 
-            type="text"
-            placeholder="Search keywords..." 
-            onChange={(value) => {setSearchTerm(value)
-                                  setVisibleCount(INCR_COUNT)}}/>
+            <Input
+                className="mb-1"
+                type="text"
+                placeholder={t('search')}
+                onChange={(value) => {
+                    setSearchTerm(value)
+                    setVisibleLimit(INCR_COUNT)
+                }} />
 
             <div className='incident-list'>
-                {props.data.filter((d, idx) => idx < visibleCount).map(function (d, idx) {
-                    if (searchTerm =="")
-                    {
-                    return (
-                        <div className='incident' key={idx}>
-                            <a className='title' href={d.url} target='_blank'>
-                                {getTitle(d)}
-                            </a>
-                            <p className='location_time'>
-                                {stateFullName(d.incident_location)} | {moment(d.incident_time).format('MM/DD/YYYY')}
-                            </p>
-                            <p className='description'>{getAbstract(d)}</p>
-                        </div>
-                    )}
-                 else if (   getTitle(d).toLowerCase().includes(searchTerm.toLowerCase())||
-                             getAbstract(d).toLowerCase().includes(searchTerm.toLowerCase())
+                {
+                    props.data.map(function (d, idx) {
+                        if (visibleCount >= visibleLimit) return;
+                        if (searchTerm == "" || getTitle(d).toLowerCase().includes(searchTerm.toLowerCase()) ||
+                            getAbstract(d).toLowerCase().includes(searchTerm.toLowerCase())) {
+                            visibleCount++;
+                            return (
+                                <div className='incident' key={idx}>
+                                    <a className='title' href={d.url} target='_blank'>
+                                        {getTitle(d)}
+                                    </a>
+                                    <p className='location_time'>
+                                        {stateFullName(d.incident_location)} | {moment(d.incident_time).format('MM/DD/YYYY')}
+                                    </p>
+                                    <p className='description'>{getAbstract(d)}</p>
+                                </div>
                             )
-                        {
-                        return (
-                            <div className='incident' key={idx}>
-                                <a className='title' href={d.url} target='_blank'>
-                                    {getTitle(d)}
-                                </a>
-                                <p className='location_time'>
-                                    {stateFullName(d.incident_location)} | {moment(d.incident_time).format('MM/DD/YYYY')}
-                                </p>
-                                <p className='description'>{getAbstract(d)}</p>
-                            </div>)
-                            }   
-                })
+                        }
+                    })
                 }
             </div>
-            {visibleCount < props.data.length ?
-                (<div align='center'><Button className='btn-loadmore' size="sm" onClick={() => setVisibleCount(visibleCount + INCR_COUNT) }>{t('load_more')}.</Button></div>)
+            {visibleLimit < props.data.length ?
+                (<div align='center'><Button className='btn-loadmore' size="sm" onClick={() => setVisibleLimit(visibleLimit + INCR_COUNT)}>{t('load_more')}.</Button></div>)
                 : null}
         </div>
     )
