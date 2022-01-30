5592adeac088dae4a8cd2d14026c090783e7b84c
Li Ma <lima01@gmail.com>
2022-01-30 00:20:51 -0500

    Disable parallel deployment. Not reliable.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 cp src/configs/config_prod.js src/configs/appConfig.js
 yarn build
 gcloud config set project hate-crime-tracker
-gsutil -m rm -r gs://hatecrimetracker/*
-gsutil -m cp -R build/* gs://hatecrimetracker
+# Do not try -m for parallel deployment. It is not reliable and often stuck.
+gsutil rm -r gs://hatecrimetracker/*
+gsutil cp -R build/* gs://hatecrimetracker
 cp src/configs/config_dev.js src/configs/appConfig.js
