c4ff7d824e707dac3f9d48f66a1d77c25d9b071a
Li Ma <lima01@gmail.com>
2022-05-07 19:28:22 -0400

    Set up project for tracker.1thing.org

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ cp src/configs/config_prod.js src/configs/appConfig.js
 yarn build
 gcloud config set project hate-crime-tracker
 # Do not try -m for parallel deployment. It is not reliable and often stuck.
-gsutil rm -r gs://hatecrimetracker/*
-gsutil cp -R build/* gs://hatecrimetracker
+gsutil rm -r gs://shortcode-tracker/*
+gsutil cp -R build/* gs://shortcode-tracker
 cp src/configs/config_dev.js src/configs/appConfig.js
