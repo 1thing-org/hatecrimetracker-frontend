740d46724e9879a9d89d2c430c9f4a63e6ff65cc
Li <lima01@gmail.com>
2022-09-13 23:18:21 -0400

    Fix bug in isObjEmpty

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -1,5 +1,5 @@
 // ** Checks if an object is empty (returns boolean)
-export const isObjEmpty = obj => Object.keys(obj).length === 0
+export const isObjEmpty = obj => !obj||Object.keys(obj).length === 0
 
 // ** Returns K format from a number
 export const kFormatter = num => (num > 999 ? `${(num / 1000).toFixed(1)}k` : num)
