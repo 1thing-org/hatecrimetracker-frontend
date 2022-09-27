0e0d49ebba407b74909980a5732c7727ab54f017
Samuel Guo <sguo114@gmail.com>
2022-09-27 19:27:12 -0400

    test ionic components

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./views/Home";
+import IncidentAdminPage from "./views/IncidentAdmin";
+import "./assets/scss/style.scss";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+// import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+import { setupIonicReact } from "@ionic/react";
+
+setupIonicReact();
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="admin" element={<IncidentAdminPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
