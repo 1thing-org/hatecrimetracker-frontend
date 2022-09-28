684006e02e9bfab46e17712699de5f521abe52e9
Samuel Guo <sguo114@gmail.com>
2022-09-28 13:50:47 -0400

    add capacitor and android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.onething.hatecrimetracker',
+  appName: 'hatecrimetracker',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
